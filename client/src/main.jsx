f78a18b1310248f63b91ce45e7b09b75b5439633
Yaroslav <6715414@gmail.com>
2023-10-06 14:11:23 +0300

    init click-market

---
+++ client/src/main.jsx
@@ -0,0 +1,10 @@
+import './index.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.querySelector('#root')).render(
+  <React.StrictMode>
+    <h1>Start</h1>
+  </React.StrictMode>
+);
